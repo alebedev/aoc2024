cb7471cf66a08fc14d502a3c956ec22b90143989
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-12-02 23:23:06 +0700

    Day 2, part 2

--- day2.py
+++ day2.py
@@ -3,25 +3,44 @@ def part1():
     safe_lines = 0
     for line in data:
         levels = [int(x) for x in line.split()]
-        first = levels.pop(0)
-        increasing = levels[0] > first
-        safe = True
-        while len(levels) > 0:
-            next = levels.pop(0)
-            diff = abs(first - next)
-            if diff < 1 or diff > 3:
-                safe = False
-                break
-            if increasing and first > next:
-                safe = False
-                break
-            if (not increasing) and first < next:
-                safe = False
-                break
-            first = next
-        if (safe):
+        if is_safe(levels.copy()):
             safe_lines += 1
-    print(safe_lines)
+    return safe_lines
+
+def is_safe(levels):
+    copy = levels.copy()
+    first = copy.pop(0)
+    increasing = copy[0] > first
+    while len(copy) > 0:
+        next = copy.pop(0)
+        diff = abs(first - next)
+        if diff < 1 or diff > 3:
+            return False
+        if increasing and first > next:
+            return False
+        if (not increasing) and first < next:
+            return False
+        first = next
+    return True
+
+def part2():
+    data = open('day2.txt').read().splitlines()
+    safe_lines = 0
+    for line in data:
+        levels = [int(x) for x in line.split()]
+        if is_safe(levels.copy()):
+            safe_lines += 1
+        else:
+            for i in range(0, len(levels)):
+                modified = levels.copy()
+                modified.pop(i)
+                if is_safe(modified):
+                    safe_lines += 1
+                    break
+    return safe_lines
+
+
 
 if __name__ == '__main__':
-    part1()
+    print(part1())
+    print(part2())
