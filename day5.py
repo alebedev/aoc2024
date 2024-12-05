ad90eb97554fce0b8e708b66768e9bd49eaa8b42
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-12-05 13:00:37 +0700

    Day5, part 2 (WIP)

--- day5.py
+++ day5.py
@@ -12,7 +12,7 @@ def part1(input: str) -> int:
             sum += req[len(req) // 2]
     return sum
 
-def parse_graph(adj_list: str) -> dict[set[int]]:
+def parse_graph(adj_list: str) -> dict[int, set[int]]:
     result = {}
     for line in adj_list.splitlines():
         a, b = map(int, line.split("|"))
@@ -30,6 +30,39 @@ def is_valid_order(order: list[str], graph: dict[set[int]]) -> bool:
             if item in graph[x]:
                 return False
     return True
+
+def part2(input: str) -> int:
+    adj_list, requests = input.split("\n\n")
+    graph = parse_graph(adj_list)
+    # print("deps: %s" % graph)
+    print("top_order: %s" % top_order(graph) )
+    sum = 0
+    master_order = {item: i for (i, item) in enumerate(top_order(graph))}
+    print("master_order: %s" % master_order)
+    for line in requests.splitlines():
+        req = [int(x) for x in line.split(",")]
+        if not is_valid_order(req, graph):
+            valid_order = sorted(req, key = lambda x: master_order[x] if x in master_order else -1)
+            print("invalid %s" % req)
+            print("valid: %s" % valid_order)
+            sum += valid_order[len(req) // 2]
+    return sum
+
+def top_order(graph: dict[int, set[int]]) -> list[int]:
+    result = []
+    visited = set()
+    def visit(x: int):
+        if x in visited:
+            return
+        visited.add(x)
+        if x in graph:
+            for n in graph[x]:
+                visit(n)
+        result.insert(0, x)
+    for key in graph.keys():
+        visit(key)
+    return result
+
 
 if __name__ == "__main__":
     test_input = textwrap.dedent("""
@@ -66,3 +99,6 @@ if __name__ == "__main__":
 
     print("Part 1 test, 143 expected: %s" % part1(test_input))
     print("Part 1: %s" % part1(input))
+
+    print("Part 2 test, 123 expected: %s" % part2(test_input))
+    print("Part 2: %s" % part2(input))
