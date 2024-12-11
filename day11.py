f9ea06f338978f8279b0b7b099b941f1fcaf4d3d
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-12-11 19:49:48 +0700

    Day 11, part 2

--- day11.py
+++ day11.py
@@ -1,14 +1,30 @@
-import textwrap
+from itertools import groupby
 
 def part1(input: str, iterations: int) -> int:
     stones = parse_input(input)
     for i in range(iterations):
         stones = step(stones)
+        # print(f"{i}: {len(stones)}")
         # print(stones)
     return len(stones)
 
-def part2(input: str):
-    pass
+def part2(input: str, iterations: int) -> int:
+    stones = normalize(parse_input(input))
+    for i in range(iterations):
+        next_stones = {}
+        for (k, count) in stones.items():
+            next = step([k])
+            for x in next:
+                next_stones[x] = next_stones.get(x, 0) + count
+        stones = next_stones
+        # print(f"{i}: {stones}")
+    sum = 0
+    for x in stones.values():
+        sum += x
+    return sum
+
+def normalize(stones: list):
+    return {k: len(list(v)) for (k,v) in groupby(sorted(stones))}
 
 def step(stones):
     result = []
@@ -30,9 +46,8 @@ def parse_input(input: str):
 
 if __name__ == "__main__":
     test_input = "125 17"
-    # input = open('day11.txt').read()
     print(f"Part 1 test, 55312 expected: {part1(test_input, 25)}")
     print(f"Part 1: {part1("814 1183689 0 1 766231 4091 93836 46", 25)}")
 
-    # print(f"Part 2 test, 81 expected: {part2(test_input)}")
-    # print(f"Part 2: {part2(input)}")
+    print(f"Part 2 test, 55312 expected: {part2(test_input, 25)}")
+    print(f"Part 2: {part2("814 1183689 0 1 766231 4091 93836 46", 75)}")
