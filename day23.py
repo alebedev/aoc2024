5d64db7c41f96e3d7832cd500bd7a1f1840e20d4
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-12-23 12:05:30 +0400

    Day 23, part 2

--- day23.py
+++ day23.py
@@ -12,7 +12,10 @@ def part1(input: str):
     return sum
 
 def part2(input: str):
-    return 0
+    pairs = parse_input(input)
+    groups = find_groups(pairs)
+    largest_group = max(groups, key=len)
+    return ','.join(sorted(largest_group))
 
 def parse_input(input: str):
     pairs = [
@@ -22,6 +25,24 @@ def parse_input(input: str):
     return pairs
 
 def find_triplets(pairs):
+    connections = find_connections(pairs)
+    computers = list(connections.keys())
+    triplets = set()
+    checked = set()
+    for i in range(len(computers)):
+        for j in range(len(computers)):
+            for k in range(len(computers)):
+                if i == j or j == k or i == k:
+                    continue
+                (a,b,c) = sorted([computers[i], computers[j], computers[k]])
+                if (a, b, c) in checked:
+                    continue
+                checked.add((a, b, c))
+                if b in connections[a] and c in connections[b] and a in connections[c]:
+                    triplets.add((a, b, c))
+    return list(triplets)
+
+def find_connections(pairs):
     # Union-find would be more efficient
     connections = {}
     def add_connection(a, b):
@@ -35,22 +56,38 @@ def find_triplets(pairs):
     for pair in pairs:
         add_connection(pair[0], pair[1])
 
-    computers = list(connections.keys())
-    triplets = set()
-    checked = set()
-    for i in range(len(computers)):
-        print(i)
-        for j in range(len(computers)):
-            for k in range(len(computers)):
-                if i == j or j == k or i == k:
+    return connections
+
+def find_groups(pairs):
+    connections = find_connections(pairs)
+    computers = set()
+    for (a,b) in pairs:
+        computers.add(a)
+        computers.add(b)
+    groups = [{c} for c in computers]
+    while True:
+        has_joined = False
+        for group_a in groups:
+            for computer in computers:
+                if computer in group_a:
                     continue
-                (a,b,c) = sorted([computers[i], computers[j], computers[k]])
-                if (a, b, c) in checked:
-                    continue
-                checked.add((a, b, c))
-                if b in connections[a] and c in connections[b] and a in connections[c]:
-                    triplets.add((a, b, c))
-    return list(triplets)
+                if all_connected(group_a, {computer}, connections):
+                    groups.remove(group_a)
+                    groups.append(group_a.union({computer}))
+                    # print('joined', group_a, computer)
+                    has_joined = True
+                    break
+        if not has_joined:
+            break
+
+    return list(groups)
+
+def all_connected(group_a, group_b, connections):
+    for a in group_a:
+        for b in group_b:
+            if b not in connections[a]:
+                return False
+    return True
 
 if __name__ == "__main__":
     test_input1 = textwrap.dedent("""
@@ -89,5 +126,7 @@ if __name__ == "__main__":
     """).strip()
     input = open("day23.txt").read()
     print(f"Part 1 test, 7 expected: {part1(test_input1)}")
-    print(f"Part 1: {part1(input)}")
+    # print(f"Part 1: {part1(input)}")
 
+    print(f"Part 2 test, 'co,de,ka,ta' expected: {part2(test_input1)}")
+    print(f"Part 2: {part2(input)}")
