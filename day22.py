bc83ef0bbddecb42bf014525533463bb865ed090
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-12-23 13:10:26 +0400

    Day 22, part 2 (slow)

--- day22.py
+++ day22.py
@@ -1,22 +1,37 @@
+import sys
 import textwrap
 
 def part1(input: str):
-    last_prices = [prices(num, 2000)[-1] for num in parse_input(input)]
+    last_prices = [gen_secrets(num, 2000)[-1] for num in parse_input(input)]
     return sum(last_prices)
 
 def part2(input: str):
-    pass
+    seeds = parse_input(input)
+    secrets = {
+        seed: gen_secrets(seed, 2000)
+        for seed in seeds
+    }
+    all_seqs = set()
+    for (seed, secrets) in secrets.items():
+        prices = [x % 10 for x in secrets]
+        diffs = [x - y for x, y in zip(prices[1:], prices)]
+        for (price, diff0, diff1, diff2, diff3) in zip(prices[4:], diffs, diffs[1:], diffs[2:], diffs[3:]):
+            seq = (diff0, diff1, diff2, diff3)
+            all_seqs.add(seq)
+    print(f"Possible sequences: {len(all_seqs)}")
+    best_seq = max(all_seqs, key=lambda seq: sum_at_seq(seeds, seq))
+    return sum_at_seq(seeds, best_seq)
 
 def parse_input(input: str):
     return [
         int(line) for line in input.splitlines()
     ]
 
-def prices(seed, count):
+def gen_secrets(seed, count):
     prices = [seed]
     for x in range(count):
         prices.append(derive(prices[-1]))
-    return prices[1:]
+    return prices
 
 def derive(seed):
     result = prune(mix(seed, seed * 64))
@@ -30,6 +45,32 @@ def mix(a, b):
 def prune(val):
     return val % 16777216
 
+def first_price_at_seq(secrets, seq):
+    prices = [x % 10 for x in secrets]
+    diffs = [x - y for x, y in zip(prices[1:], prices)]
+
+    for (price, diff0, diff1, diff2, diff3) in zip(prices[4:], diffs,diffs[1:],diffs[2:],diffs[3:]):
+        seq_at_price = (diff0, diff1, diff2, diff3)
+        # print(f"{price}: {seq_at_price}")
+        if seq_at_price == seq:
+            return price
+    return 0
+
+secrets_cache = {}
+def sum_at_seq(seeds, seq):
+    sum = 0
+    for x in seeds:
+        secrets = secrets_cache.get(x)
+        if not secrets:
+            secrets = gen_secrets(x, 2000)
+            secrets_cache[x] = secrets
+        price = first_price_at_seq(secrets, seq)
+        # print(f"{x}: {price}")
+        sum += price
+    sys.stdout.write(".")
+    return sum
+
+
 if __name__ == "__main__":
     test_input1 = textwrap.dedent("""
     1
@@ -38,6 +79,17 @@ if __name__ == "__main__":
     2024
     """).strip()
     input = open("day22.txt").read()
-    print(f"Prices from 123: {prices(123, 10):}")
+    print(f"Prices from 123: {gen_secrets(123, 11):}")
     print(f"Part 1 test, 37327623 expected: {part1(test_input1)}")
-    print(f"Part 1: {part1(input)}")
+    # print(f"Part 1: {part1(input)}")
+
+    test_input2 = textwrap.dedent("""
+    1
+    2
+    3
+    2024
+    """).strip()
+    print(f"Prices from 123: {[x % 10 for x in gen_secrets(123, 10)]}")
+    print(f"Part 2 test, 23 expected: {sum_at_seq(parse_input(test_input2),  (-2,1,-1,3))}")
+    # print(f"Part 2 test 2, 23 expected: {part2(test_input2)}")
+    print(f"Part 2: {part2(input)}")
